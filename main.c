286d930576cb750c17c4c997df8d05919e15889c
lsWorl <1428903832@qq.com>
2026-01-09 13:43:29 +0800

    encryption

--- main.c
+++ main.c
@@ -3,26 +3,210 @@
 #include <string.h>
 
 typedef unsigned char byte; //定义byte类型
-#define Nb 4           //块大小（以32位字为单位）
-#define Nk 4           //密钥长度（以32位字为单位）
+#define Nb 4           //列数
+#define Nk 4           //密钥长度,以32位字为单位,故当前密钥长度为128位
 #define Nr 10          //轮数
+#define STATE_SIZE 16 //状态矩阵大小
+byte state[4][4]; //状态矩阵
+static const byte Rcon[11] = {  //轮常量
+    0x00,
+    0x01, 0x02, 0x04, 0x08,
+    0x10, 0x20, 0x40, 0x80,
+    0x1B, 0x36
+};
+static const byte Sbox[256] = {
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+};
+
+
+
+
+//将输入数据填充到状态矩阵和轮密钥矩阵中
+void init_state(byte input[STATE_SIZE]){
+    int c, r;  // c 为列, r 为行
+    for(c = 0; c < 4; c++){
+        for(r = 0; r < 4; r++){
+            state[r][c] = input[r + 4 * c];
+        }
+    }
+}
+
+//字节替代操作
+void SubBytes(){
+    int i,j;
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            state[i][j] = Sbox[state[i][j]]; //使用S盒进行替代
+        }
+    }
+}
+
+//列移位操作
+void shift_rows(){
+    byte temp[4][4];
+    int i,j;
+    //复制状态矩阵到临时矩阵
+    for(i = 0; i < 4; i++){
+        for (j = 0; j < 4; j++){
+            temp[i][j] = state[i][j];
+        }
+    }
+    //进行行移位操作
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            state[i][j] = temp[i][(j + i) % 4];
+        }
+    }
+}
+
+//xtime函数：在GF(2^8)上乘以2,用于实现x2的部分
+byte xtime(byte x) {
+    return (x << 1) ^ ((x & 0x80) ? 0x1b : 0x00); //若最高位为1则需要再异或上0x1b
+}
+
+//乘03的情况
+byte mul_by_03(byte x) {
+    return xtime(x) ^ x; //x3 = x2 + x1
+}
+
+//列混合
+void MixColumns(){
+    byte temp[4][4];
+    int i, j;
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            temp[i][j] = state[i][j];
+        }
+    }
+    //进行列混合操作 01为x1 02为x2 03为x2+x1
+    for(i = 0; i < 4; i++){
+        state[0][i] = xtime(temp[0][i]) ^ mul_by_03(temp[1][i]) ^ temp[2][i] ^ temp[3][i];
+        state[1][i] = temp[0][i] ^ xtime(temp[1][i]) ^ mul_by_03(temp[2][i]) ^ temp[3][i];
+        state[2][i] = temp[0][i] ^ temp[1][i] ^ xtime(temp[2][i]) ^ mul_by_03(temp[3][i]);
+        state[3][i] = mul_by_03(temp[0][i]) ^ temp[1][i] ^ temp[2][i] ^ xtime(temp[3][i]);
+    }
+}
+
+//字循环操作
+void rot_word(byte* word){
+    byte temp = word[0];
+    word[0] = word[1];
+    word[1] = word[2];
+    word[2] = word[3];
+    word[3] = temp;
+}
+
+//字节替代操作
+void sub_word(byte* word){
+    int i;
+    for(i = 0; i < 4; i++){
+        word[i] = Sbox[word[i]];
+    }
+}
+
+//密钥扩展 密钥扩展后得到44个字(4字/轮 * (10轮 +1))
+void key_expansion(byte key[STATE_SIZE], byte roundKeys[44][4]){
+    int i;
+    //将初始密钥复制到前4个字
+    for(i = 0; i < Nk; i++){
+        roundKeys[i][0] = key[4*i];
+        roundKeys[i][1] = key[4*i + 1];
+        roundKeys[i][2] = key[4*i + 2];
+        roundKeys[i][3] = key[4*i + 3];
+    }
+    //生成后续的轮密钥
+    for(i = Nk; i < Nb * (Nr + 1); i++){
+        byte temp[4];
+        temp[0] = roundKeys[i - 1][0];
+        temp[1] = roundKeys[i - 1][1];
+        temp[2] = roundKeys[i - 1][2];
+        temp[3] = roundKeys[i - 1][3];
+        //若当前字是Nk的倍数，则进行字循环和字节替代，并与轮常量异或
+        if(i % Nk == 0){
+            rot_word(temp);
+            sub_word(temp);
+            //仅第一行与轮常量异或
+            temp[0] ^= Rcon[i / Nk];
+        }
+        //与前Nk个字进行异或，生成当前字
+        roundKeys[i][0] = roundKeys[i - Nk][0] ^ temp[0];
+        roundKeys[i][1] = roundKeys[i - Nk][1] ^ temp[1];
+        roundKeys[i][2] = roundKeys[i - Nk][2] ^ temp[2];
+        roundKeys[i][3] = roundKeys[i - Nk][3] ^ temp[3];
+    }
+}
+
+//轮密钥加操作
+void add_round_key(byte roundKeys[44][4], int round){
+    int c,r;
+    for(c = 0; c < 4; c++){
+        for(r = 0; r < 4; r++){
+            state[r][c] ^= roundKeys[round * Nb + c][r];
+        }
+    }
+}
+
+//打印信息
+void print_state(){
+    int i,j;
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            printf("%02x ", state[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 
 int main(void) {
-    printf("AES Encryption/Decryption\n");
+    //对应论文地址为https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197.pdf
+    //状态向量采用Appendix B中的测试向量
+    byte input[STATE_SIZE] = {
+    0x32, 0x43, 0xf6, 0xa8,
+    0x88, 0x5a, 0x30, 0x8d,
+    0x31, 0x31, 0x98, 0xa2,
+    0xe0, 0x37, 0x07, 0x34
+};
+    byte key[STATE_SIZE] = {
+    0x2b, 0x7e, 0x15, 0x16,
+    0x28, 0xae, 0xd2, 0xa6,
+    0xab, 0xf7, 0x15, 0x88,
+    0x09, 0xcf, 0x4f, 0x3c
+    };
+
+    init_state(input);
+    print_state();
+
+    SubBytes();
+    //打印S盒替换后结果
+    printf("\nAfter SubBytes:\n");
     
-    // Placeholder for AES implementation
-    uint8_t plaintext[16] = {0};
-    uint8_t key[16] = {0};
-    uint8_t ciphertext[16] = {0};
-    
-    printf("Plaintext: ");
-    for (int i = 0; i < 16; i++) {
-        printf("%02x ", plaintext[i]);
-    }
-    printf("\n");
-    
-    // TODO: Add AES encryption/decryption calls here
-    
+    print_state();
+
+    //打印列移位操作后结果
+    shift_rows();
+    printf("\nAfter ShiftRows:\n");
+    print_state();
+
+    //打印列混合操作后结果
+    MixColumns();
+    printf("\nAfter MixColumns:\n");
+    print_state();
+
     return 0;
 }
